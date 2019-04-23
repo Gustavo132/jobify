bbfdc213509a50a31a65ef677e8dd5466a3753f3
Gustavo132 <gustavo132ish@gmail.com>
2019-04-23 20:51:19 -0300

    changing variable port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path')
 const sqlite = require('sqlite')
 const dbConnection = sqlite.open(path.relative(__dirname, 'banco.sqlite'), { Promise })
 
-const port = process.env.PORT || 3000
+const port = process.env.port || 3000
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
